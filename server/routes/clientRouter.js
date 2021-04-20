4cd6d113acc48a6e10d23ed5d6b8f954f8bb6ebc
lenapuzzle <lenapuzzle@yahoo.com>
2021-04-20 08:56:42 -0700

    fixed bugs for backend

--- server/routes/clientRouter.js
+++ server/routes/clientRouter.js
@@ -1,9 +1,12 @@
 import express from "express"
+
 const router = new express.Router()
-​
-const clientRoutes = []
+
+const clientRoutes = [] 
+// const clientRoutes = ["/pets/:petType"] 
+
 router.get(clientRoutes, (req, res) => {
   res.render("home")
 })
-​
+
 export default router
