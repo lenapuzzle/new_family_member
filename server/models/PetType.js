50d4f1b70a95a993fe31346403994acc7510c1f1
benjamin-z-l <you@example.com>
2021-04-21 11:27:07 -0400

    fixed indentation

--- server/models/PetType.js
+++ server/models/PetType.js
@@ -3,44 +3,41 @@ import pg from "pg"
 const pool = new pg.Pool({ connectionString: "postgres://postgres:password@localhost:5432/new_family_member_development" })
 
 class PetType {
-    constructor({ id, type, img_url, imgUrl, description }) {
-        this.id = id
-        this.type = type
-        this.imgUrl = imgUrl || img_url
-        this.description = description
+  constructor({ id, type, img_url, imgUrl, description }) {
+    this.id = id
+    this.type = type
+    this.imgUrl = imgUrl || img_url
+    this.description = description
+  }
+
+  static async findAll() {
+    try {
+      const result = await pool.query("SELECT * FROM pet_types;")
+      const typesData = result.rows
+      const types = typesData.map(type => new this(type))
+
+      return types
+
+    } catch (error) {
+      console.log(error)
+      throw (error)
     }
-    static async findAll() {
-        try {
-            const result = await pool.query("SELECT * FROM pet_types;")
-            const typesData = result.rows
-            const types = typesData.map(type => new this(type))
+  }
 
-            return types
-
-        } catch (error) {
-            console.log(error)
-            throw (error)
-        }
+  static async findPetsByType(type) {
+    const petFile = await import('./Pet.js')
+    const Pet = petFile.default
+    try {
+      const query = "SELECT * FROM adoptable_pets JOIN pet_types ON pet_type_id = pet_types.id WHERE type = $1;"
+      const result = await pool.query(query, [type])
+      const petsData = result.rows
+      const pets = petsData.map(pet => new Pet(pet))
+      return pets
+    } catch (error) {
+      console.log(error)
+      throw (error)
     }
-
-    static async findPetsByType(type) {
-        const petFile = await import('./Pet.js')
-        const Pet = petFile.default
-
-        try {
-            const query = "SELECT * FROM adoptable_pets JOIN pet_types ON pet_type_id = pet_types.id WHERE type = $1;"
-            const result = await pool.query(query, [type])
-            const petsData = result.rows
-            const pets = petsData.map(pet => new Pet(pet))
-            return pets
-        } catch (error) {
-            console.log(error)
-            throw (error)
-        }
-    }
-
-    static async findById(Id) {
-    }
-
+  }
 }
+
 export default PetType
