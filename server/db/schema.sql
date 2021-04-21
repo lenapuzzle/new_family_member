07087c1270b46a62c30b28956ad090000593ffdf
benjamin-z-l <you@example.com>
2021-04-21 15:17:31 -0400

    updated complete schema.sql for app

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,20 +1,41 @@
 DROP TABLE IF EXISTS pet_types CASCADE;
+DROP TABLE IF EXISTS surrender_applications CASCADE;
+DROP TABLE IF EXISTS adoption_applications CASCADE;
 DROP TABLE IF EXISTS adoptable_pets;
 
 CREATE TABLE pet_types (
-    id SERIAL PRIMARY KEY,
-    type VARCHAR(255) NOT NULL,
-    img_url VARCHAR(255) NOT NULL,
-    description TEXT
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(255) NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  description TEXT
 );
 
 CREATE TABLE adoptable_pets (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    img_url VARCHAR(255) NOT NULL,
-    age INTEGER,
-    vaccination_status BOOLEAN NOT NULL,
-    adoption_story TEXT NOT NULL,
-    available_for_adoption BOOLEAN NOT NULL,
-    pet_type_id INTEGER REFERENCES pet_types
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  age INTEGER,
+  vaccination_status BOOLEAN NOT NULL,
+  adoption_story TEXT NOT NULL,
+  available_for_adoption BOOLEAN NOT NULL,
+  pet_type_id INTEGER REFERENCES pet_types
+);
+
+CREATE TABLE surrender_applications (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  application_status VARCHAR(255) DEFAULT 'pending',
+  adoptable_pet_id INTEGER REFERENCES adoptable_pets
+);
+
+CREATE TABLE adoption_application (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  home_status VARCHAR(255) NOT NULL,
+  application_status VARCHAR(255) DEFAULT 'pending',
+  adoptable_pet_id INTEGER REFERENCES adoptable_pets
 );
