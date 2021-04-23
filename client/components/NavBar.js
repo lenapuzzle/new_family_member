409091141fddef6a28cc6ba883ee8bf21538ec8c
lenapuzzle <lenapuzzle@yahoo.com>
2021-04-22 21:01:37 -0700

    navbar work

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import { Switch, Route, Link } from "react-router-dom"
 
 import PetTypeIndex from "./PetTypeIndex"
@@ -6,19 +6,48 @@ import PetIndex from "./PetIndex"
 import SurrenderForm from "./SurrenderForm"
 
 const NavBar = props => {
+  const [petTypes, setPetTypes] = useState([])
+  useEffect(() => {
+    const fetchPetTypes = async () => {
+      try {
+        const response = await fetch("/api/v1/pets")
+        if (!response.ok) {
+          const errorMessage = `${response.status} (${response.statusText})`
+          const error = new Error(errorMessage)
+          throw (error)
+        }
+        const petTypeData = await response.json()
+        setPetTypes(petTypeData.petTypes)
+      } catch (err) {
+        console.error(`Error in fetch: ${err.message}`)
+      }
+    }
+    fetchPetTypes()
+  }, [])
+
+  const petTypeLinks = petTypes.map(petType => {
+    let typeName = petType.type.charAt(0).toUpperCase() + petType.type.slice(1)
+    return (
+      <div>
+        <Link to={`/pets/${petType.type}`}>Adorable {typeName}</Link>
+      </div>
+    )
+  })
+
   return (
     <div className="row column">
       <div className="navbar">
         <Link to="/pets">All Pets</Link>
       </div>
-      <div className="navbar">
+      {/* <div className="navbar">
         <Link to="/pets/cats">Precious Cats</Link>
       </div>
       <div className="navbar">
         <Link to="/pets/dogs">Adorable Dogs</Link>
-      </div>
+      </div> */}
+      {petTypeLinks}
       <div className="navbar">
-        <Link to="/adoptions/new">Adopt Me</Link>
+        <Link to="/adoptions/new">Sad to say Goodbye</Link>
       </div>
       <Switch>
         <Route exact path="/pets" component={PetTypeIndex} />
